a42e98e7ff4edadf1bcee66aa671980bddc67c7e
German <germants100@gmail.com>
2024-04-16 22:35:47 -0600

    Coding and styling the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className={styles.navbar}>
+        <a className={styles.title} href="/">
+            Portfolio
+        </a>
+        <div className={styles.menu}>
+            <ul className={styles.menuItems}>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
   )
 }
 

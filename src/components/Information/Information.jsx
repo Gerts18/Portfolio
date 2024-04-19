fafdd7d9e6c20cfceaabc18b75081ae7790013c0
German <germants100@gmail.com>
2024-04-19 00:31:48 -0600

    Adding some styles and props

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './Information.module.css'
 import { getImageUrl } from '../../utils'
 
-const Information = ({title}) => {
+const Information = ({title, description, path}) => {
   return (
-    <section>
-        <img src={getImageUrl('about/uilcon.png')} alt="" />
-        <div>
-            <h1>{title}</h1>
+    <section className={styles.container}>
+        <img className={styles.image} src={getImageUrl(path)} alt="" />
+        <div className={styles.textContainer}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.description}>{description} </p>
         </div>
     </section>
   )

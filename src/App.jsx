a21ff37e4aefaa485ebd8f9acd77ea3a8231149d
German <germants100@gmail.com>
2024-04-22 16:38:23 -0600

    Creating Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
+import Projects from './components/Projects/Projects'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Hero/>
         <About/>
         <Experience/>
+        <Projects/>
       </div>
     </>
   )

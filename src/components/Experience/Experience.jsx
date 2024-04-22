63c708c53bc32787c7b1d4d4cf4ea697ffad0f87
German <germants100@gmail.com>
2024-04-21 23:02:39 -0600

    Styling Experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 import skills from '../../data/skills.json'
 import history from '../../data/history.json'
 import { getImageUrl } from '../../utils'
+import style from './Experience.module.css'
 
 const Experience = () => {
   return (
-    <section id='experience'>
-        <h2>Experience</h2>
-        <div>
-            <div>
+    <section className={style.container} id='experience'>
+        <h2 className={style.title}>Experience</h2>
+        <div className={style.content}>
+            <div className={style.skillsContainer}>
                 {
                     skills.map((skill, id) => { 
                          return (
-                            <div key={id}>{/* When you iterate trought an array in React, for create elements, you need to give a key for each one of them */}
-                            <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
+                            <div className={style.skill} key={id}>{/* When you iterate trought an array in React, for create elements, you need to give a key for each one of them */}
+                            <div className={style.skillImageContaniner}><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
                             <p>{skill.title} </p>
                             </div>
                          )
@@ -22,13 +23,13 @@ const Experience = () => {
                 }
             </div>
             <div>
-                <ul>
+                <ul className={style.experiencesContaniner}>
                   {
                     history.map((experience, id) => {
                       return (
-                        <li key={id} >
+                        <li className={style.historyItem} key={id} >
                           <img src={getImageUrl(experience.imageSrc)} alt= {`${experience.organization} logo`} />
-                          <div>
+                          <div className={style.historyItemDetails}>
                             <h3>{`${experience.role}, ${experience.organization}`} </h3>
                             <p>
                             {`${experience.startDate}-${experience.endDate}`}

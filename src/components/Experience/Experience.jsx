525f9c67519f392b1a832f28b9b9c10d15422eaf
German <germants100@gmail.com>
2024-04-21 19:33:58 -0600

    Importing data from skills json file with map function

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import skills from '../../data/skills.json'
+import { getImageUrl } from '../../utils'
+
 const Experience = () => {
   return (
-    <section>
-        
+    <section id='experience'>
+        <h2>Experience</h2>
+        <div>
+            <div>
+                {
+                    skills.map((skill, id) => { 
+                         return (
+                            <div key={id}>{/* When you iterate trought an array in React, for create elements, you need to give a key for each one of them */}
+                            <div><img src={getImageUrl(skill.imageSrc)} alt={skill.title} /></div>
+                            <p>{skill.title} </p>
+                            </div>
+                         )
+                     })
+                }
+            </div>
+            <div>
+                
+            </div>
+        </div>
     </section>
   )
 }

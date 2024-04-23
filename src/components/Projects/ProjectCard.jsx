2774d1c9b867ce444e936636d03903bf8ba80fbd
German <germants100@gmail.com>
2024-04-22 21:48:05 -0600

    Changing the project section

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -5,11 +5,11 @@ import style from './ProjectCard.module.css'
 const ProjectCard = ({project : {title, imageSrc, description, skills, demo, source}}) => {
     return(
         <div className={style.container}>
-            <div>
+            <div className={style.items}>
             <h3 className={style.title}>{title} </h3>
             <img className={style.image} src={getImageUrl(imageSrc)} alt={`image of ${title}`} />
             </div>
-            <div>
+            <div className={style.descriptiveItems}>
             <p className={style.description}>{description}</p>
             <ul className={style.skills}>
                 {
@@ -24,7 +24,7 @@ const ProjectCard = ({project : {title, imageSrc, description, skills, demo, sou
             </ul>
             <div className={style.links}>
                 <a className={style.link} href={demo} target='_blank'>Demo</a>
-                <a className={style.link} href={source} target='_blank'>Source</a>
+                <a className={style.link} href={} target='_blank'>Source</a>
             </div>
             </div>
         </div>

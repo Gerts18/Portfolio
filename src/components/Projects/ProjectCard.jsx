540249a759d232da42895a8a5b55c2ca8a276560
German <germants100@gmail.com>
2024-06-14 09:45:52 -0600

    Editing project section and title of hero section

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,26 +6,29 @@ const ProjectCard = ({project : {title, imageSrc, description, skills, demo, sou
     return(
         <div className={style.container}>
             <div className={style.items}>
-            <h3 className={style.title}>{title} </h3>
-            <img className={style.image} src={getImageUrl(imageSrc)} alt={`image of ${title}`} />
+                <h3 className={style.title}>{title} </h3>
+                <img className={style.image} src={getImageUrl(imageSrc)} alt={`image of ${title}`} />
             </div>
+
             <div className={style.descriptiveItems}>
-            <p className={style.description}>{description}</p>
-            <ul className={style.skills}>
-                {
-                    skills.map((skill,id) => {
-                        return (
-                            <li className={style.skill} key={id}>
-                                {skill}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-            <div className={style.links}>
-                <a className={style.link} href={source} target='_blank'>Source</a>
+                <p className={style.description}>{description}</p>
+                <ul className={style.skills}>
+                    {
+                        skills.map((skill,id) => {
+                            return (
+                                <li className={style.skill} key={id}>
+                                    {skill}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <div className={style.links}>
+                    <a className={style.link} href={source} target='_blank'>Source</a>
+                    <a className={style.link} href={demo} target='_blank'>Demo</a>
+                </div>
             </div>
-            </div>
+
         </div>
     )
 }
